926aaeb7857796ca7e6e072b23aa4157848bc244
Ummaro <jdamas.2624@gmail.com>
2026-04-29 16:35:33 +0200

    feat: added session, authentication mostly done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.Server import Server
 
 if __name__ == "__main__":
-    server = Server(dev=False)
-    server.run()
+    server = Server(dev=True)
+    server_thread = server.run()
+    if server_thread:
+        server_thread.join()
--- src/Server.py
+++ src/Server.py
@@ -1,12 +1,16 @@
 import flask
 import threading
 import time
+import os
+from dotenv import load_dotenv
 from waitress import serve
 from src.Strava import Strava
 
 class Server:
     def __init__(self, dev=True):
+        load_dotenv()
         self.app = flask.Flask(__name__)
+        self.app.secret_key = os.getenv("SECRET_KEY")
         self.strava = Strava(dev=dev)
         self.dev = dev
         
@@ -28,9 +32,11 @@ class Server:
             if not code:
                 return "Missing code", 400
 
-            success = self.strava.authenticate(code=code)
-            if success:
-                return "Authentication successful", 200
+            client = self.strava.authenticate(code=code)
+            if client:
+                athlete_id = client.get_athlete().id
+                flask.session['athlete_id'] = athlete_id
+                return flask.redirect("/dashboard")
             else:
                 return "Authentication failed", 500
             
@@ -39,17 +45,28 @@ class Server:
             url = self.strava.generate_url()
             return flask.redirect(url)
         
+        @self.app.route("/dashboard")
+        def dashboard():
+            athlete_id = flask.session.get('athlete_id')
+            if not athlete_id:
+                return flask.redirect("/login")
+            
+        @self.app.route("/login")
+        def login():
+            return "Please authenticate <a href='/authenticate'>here</a>"
+        
     def run(self):
             if self.dev:
                 thread = threading.Thread(
                     target=serve, 
                     args=(self.app,), 
                     kwargs={'host': self.host, 'port': self.port, '_quiet': True},
-                    daemon=True
+                    daemon=True,
                 )
                 thread.start()
                 time.sleep(1)
                 print(f"Serveur de TEST démarré sur {self.host}:{self.port}")
+                return thread
             else:
                 print(f"Serveur PROD démarré sur {self.host}:{self.port}")
-                serve(self.app, host=self.host, port=self.port)
+                return serve(self.app, host=self.host, port=self.port)
