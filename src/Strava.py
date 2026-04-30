fe3b73b1973eae315be7ff7189f665a61d1f3ebe
Ummaro <jdamas.2624@gmail.com>
2026-04-30 11:46:51 +0200

    fix: revert athlete id auth

--- src/Strava.py
+++ src/Strava.py
@@ -27,7 +27,7 @@ class Strava:
             self.rate_limiter = DefaultRateLimiter(priority="low")
             self.db = Database(os.path.join(self.project_root, "strava_prod.db"))
 
-    def authenticate(self, code=None):
+    def authenticate(self, code=None, athlete_id=None):
         client = Client(rate_limiter=self.rate_limiter)
         token_data = None
 
@@ -37,6 +37,8 @@ class Strava:
                 client_secret=self.client_secret, 
                 code=code
             )
+        elif athlete_id:
+            token_data = self.db.get_token_by_athlete_id(athlete_id)
         elif self.dev:
             token_data = self._load_local_token()
 
@@ -50,8 +52,8 @@ class Strava:
         client.refresh_token = token_data["refresh_token"]
         client.token_expires = token_data["expires_at"]
 
-
-        athlete_id = client.get_athlete().id
+        if not athlete_id:
+            athlete_id = client.get_athlete().id
 
         if time.time() >= client.token_expires:
             token_data = self.refresh_token(client, athlete_id)
